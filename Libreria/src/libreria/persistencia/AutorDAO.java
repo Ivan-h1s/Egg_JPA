95a4aa45dd567199d450d75f8e092bfb524f74e5
Ivan Altamirano <pedrotrill@gmail.com>
2023-01-19 05:35:14 -0300

    2c

--- Libreria/src/libreria/persistencia/AutorDAO.java
+++ Libreria/src/libreria/persistencia/AutorDAO.java
@@ -1,36 +1,84 @@
 package libreria.persistencia;
 
 import java.util.List;
-//import javax.persistence.EntityManager;
-//import javax.persistence.Persistence;
+
 import libreria.entidades.Autor;
 
 public class AutorDAO extends DAO<Autor>{
     
     @Override
-    public void guardarAutor(Autor autor) {
-        if (autor == null) {
-            System.out.println("Debe indicar un autor");
-        } else {            
-        super.guardar(autor);
-            System.out.println("Autor guardado");
-        }
+    public void guardar(Autor autor) {
+        try {
+            if (autor == null) {
+                System.out.println("Debe indicar un autor.");
+            }
+            super.guardar(autor);
+            System.out.println("Autor guardado.");
+        } catch (Exception e) { 
+            System.out.println("Error al crear un autor.");
+            throw e;             
+        } finally {
+            desconectar();
+        }  
+    }   
+        
+    public Autor buscarPorId(Integer id) throws Exception {
+        conectar();
+        Autor idAutor = null;
+        try {
+            idAutor = (Autor) em.find(Autor.class, id);
+            return idAutor;
+        } catch (Exception e) {            
+            System.out.println("No se encontró el ID del autor.");
+            throw e;        
+        } finally {
+            desconectar();
+        }        
     }
     
-//    public Autor buscarPorId(String id) throws Exception {
-//        conectar();
-//        Autor autor = em.find(Autor.class, id);
-//        desconectar();
-//        return autor;
-   // }
+    public List<Autor> buscarPorNombre(String nombre) {
+        conectar();
+        List<Autor> listaAutor = null;
+        try {
+            listaAutor = em.createQuery("SELECT a FROM Autor a WHERE a.nombre LIKE :nombre")
+                                   .setParameter("nombre", nombre).getResultList();
+            return listaAutor;
+        } catch (Exception e) {
+            System.out.println("No se encontró el nombre del autor.");
+            throw e;        
+        } finally {
+            desconectar();
+        }    
+    }
     
+    public List<Autor> listarTodosAutores() throws Exception {
+        conectar();
+        List<Autor> getAllAutors = null;
+        try {
+            getAllAutors = em.createQuery("SELECT a FROM Autor a").getResultList();
+            return getAllAutors;
+        } catch (Exception e) { 
+            System.out.println("No hay autores.");
+            throw e;        
+        } finally {
+            desconectar();
+        }               
+    }
     
-//    public void eliminar(String id) throws Exception {
-//        Autor autor = bus
-//    }
-//    public List<Autor> buscarPorNombre(String nombre) {
-//        List<Autor> listaAutor = em.createQuery("SELECT a FROM Autor a WHERE a.nombre LIKE :nombre")
-//                                   .setParameter("nombre", nombre).getResultList();
-//        return listaAutor;
-//    }
+    public void eliminar(Integer id) throws Exception{
+        Autor autor = null;
+        try {
+            if (autor == null) {
+                System.out.println("Debe indicar un autor.");
+            }
+            autor = buscarPorId(id);
+            super.eliminar(autor);
+        } catch (Exception e) {            
+            System.out.println("Error al eliminar autor.");
+            throw e;        
+        } finally {
+            desconectar();
+        }        
+    }
+    
 }
---
+++ Libreria/src/libreria/persistencia/DAO.java
@@ -0,0 +1,67 @@
+// Esta clase padre nos permite heredar métodos comunes con el fin de reutilizar código
+package libreria.persistencia;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class DAO<T> {
+    
+    protected EntityManagerFactory EMF = Persistence.createEntityManagerFactory("libreriaPU");
+    protected EntityManager em = EMF.createEntityManager();
+    
+    // método para conectar con la base de datos.
+    // verifica si la conexión está realizada, en caso contrario, se realiza en la línea 18
+    protected void conectar() {
+        if(!em.isOpen()) {
+            em = EMF.createEntityManager();
+        }
+    }
+    
+    // método para desconectarnos de la base de datos
+    // verifica si existe una conexión, y de ser el caso, se 
+    // cierra la conexión y se desconecta el programa con la base de datos.    
+    protected void desconectar() {
+        if (em.isOpen()) {
+            em.close();
+        }
+    }
+    
+    // método para persistir un objeto en la base de datos.
+    // Toma como parámetro el objeto a persistir, vemos que es genérico, por
+    // lo que puede aceptar cualquier tipo de objeto (Autor, LIbro, Editorial)
+    protected void guardar(T objeto) {//create
+        conectar();
+        em.getTransaction().begin();
+        em.persist(objeto);
+        
+        em.getTransaction().commit();       
+        desconectar();       
+    }
+    
+    // método para modificar una tupla de una base de datos.
+    // Recibe como parámetro el objeto con los datos cambiados (debe mantener
+    // la misma llave primaria) y lo reemplaza por el anterior.
+    protected void editar(T objeto) {//update
+        conectar();
+        em.getTransaction().begin();
+        em.merge(objeto);
+        
+        em.getTransaction().commit();
+        desconectar();
+    }
+    
+    // método para eliminar un registro de la base de datos.
+    // Como parámetro se pasa el objeto a eliminar de la base de datos.
+    // Se busca en la base de datos el registro que contenga la misma información
+    // que el parámetro recibido, y se elimina.
+    protected void eliminar(T objeto) {//delete
+        conectar();
+        em.getTransaction().begin();
+        em.remove(objeto);
+        
+        em.getTransaction().commit();
+        desconectar();
+    }
+
+}
