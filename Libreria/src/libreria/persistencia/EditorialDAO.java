5e7123009ca2be269a4da7d4095c7a4b63827dd4
Ivan Altamirano <pedrotrill@gmail.com>
2023-01-20 04:52:22 -0300

    persistencias

--- Libreria/src/libreria/persistencia/EditorialDAO.java
+++ Libreria/src/libreria/persistencia/EditorialDAO.java
@@ -1,5 +1,82 @@
 package libreria.persistencia;
 
-public class EditorialDAO {
+import java.util.List;
+import libreria.entidades.Editorial;
+
+public class EditorialDAO extends DAO<Editorial>{
     
+    @Override
+    public void guardar(Editorial editorial) {
+        try {
+            if (editorial == null) {
+                System.out.println("Debe indicar una editorial.");
+            }
+            super.guardar(editorial);
+            System.out.println("Editorial guardada.");
+        } catch (Exception e) { 
+            System.out.println("Error al crear una editorial.");
+            throw e;             
+        } finally {
+            desconectar();
+        }  
+    }  
+    
+    public Editorial buscarEditPorId(Integer id) throws Exception {
+        conectar();
+        Editorial idEditorial = null;
+        try {
+            idEditorial = (Editorial) em.find(Editorial.class, id);
+            return idEditorial;
+        } catch (Exception e) {            
+            System.out.println("No se encontró el ID de la editorial.");
+            throw e;        
+        } finally {
+            desconectar();
+        }        
+    }
+    
+    public List<Editorial> buscarEditPorNombre(String nombre) {
+        conectar();
+        List<Editorial> listaEditorial = null;
+        try {
+            listaEditorial = em.createQuery("SELECT a FROM Editorial a WHERE a.nombre LIKE :nombre")
+                                   .setParameter("nombre", nombre).getResultList();
+            return listaEditorial;
+        } catch (Exception e) {
+            System.out.println("No se encontró el nombre de la editorial.");
+            throw e;        
+        } finally {
+            desconectar();
+        }    
+    }
+    
+    public List<Editorial> listarTodasEditoriales() throws Exception {
+        conectar();
+        List<Editorial> allEditorials = null;
+        try {
+            allEditorials = em.createQuery("SELECT a FROM Editorial a").getResultList();
+            return allEditorials;
+        } catch (Exception e) { 
+            System.out.println("No hay editoriales.");
+            throw e;        
+        } finally {
+            desconectar();
+        }               
+    }
+    
+    public void eliminarEdit(Integer id) throws Exception{
+        Editorial editorial = null;
+        try {
+            if (editorial == null) {
+                System.out.println("Debe indicar una editorial.");
+            }
+            editorial = buscarEditPorId(id);
+            super.eliminar(editorial);
+        } catch (Exception e) {            
+            System.out.println("Error al eliminar editorial.");
+            throw e;        
+        } finally {
+            desconectar();
+        }        
+    }
 }
--- Libreria/src/libreria/persistencia/LibroDAO.java
+++ Libreria/src/libreria/persistencia/LibroDAO.java
@@ -1,5 +1,113 @@
 package libreria.persistencia;
 
-public class LibroDAO {
+import java.util.List;
+import libreria.entidades.Libro;
+
+public class LibroDAO extends DAO<Libro>{
+    
+    @Override
+    public void guardar(Libro libro) {
+        try {
+            if (libro == null) {
+                System.out.println("Debe indicar el nombre del libro.");
+            }
+            super.guardar(libro);
+            System.out.println("Libro guardado.");
+        } catch (Exception e) { 
+            System.out.println("Error al crear un libro.");
+            throw e;             
+        } finally {
+            desconectar();
+        }  
+    }  
+    
+    public Libro buscarPorIsbn(Integer isbn) throws Exception {
+        conectar();
+        Libro libro = null;
+        try {
+            libro = (Libro) em.find(Libro.class, isbn);
+            return libro;
+        } catch (Exception e) {            
+            System.out.println("No se encontró el ISBN del libro.");
+            throw e;        
+        } finally {
+            desconectar();
+        }        
+    }
+    
+    public List<Libro> buscarLibroPorTitulo(String titulo) {
+        conectar();
+        List<Libro> listaTitulos = null;
+        try {
+            listaTitulos = em.createQuery("SELECT a FROM Libro a WHERE a.titulo LIKE :nombre")
+                                   .setParameter("nombre", titulo).getResultList();
+            return listaTitulos;
+        } catch (Exception e) {
+            System.out.println("No se encontró el titulo del libro.");
+            throw e;        
+        } finally {
+            desconectar();
+        }    
+    }
+    
+    public List<Libro> listarTodosLibros() throws Exception {
+        conectar();
+        List<Libro> books = null;
+        try {
+            books = em.createQuery("SELECT a FROM Libro a").getResultList();
+            return books;
+        } catch (Exception e) { 
+            System.out.println("No hay libros.");
+            throw e;        
+        } finally {
+            desconectar();
+        }               
+    }
+    
+    public void eliminarLibro(Integer isbn) throws Exception{
+        Libro libro = null;
+        try {
+            if (libro == null) {
+                System.out.println("Debe indicar un isbn.");
+            }
+            libro = buscarPorIsbn(isbn);
+            super.eliminar(libro);
+        } catch (Exception e) {            
+            System.out.println("Error al eliminar un libro.");
+            throw e;        
+        } finally {
+            desconectar();
+        }        
+    }
+    
+    public List<Libro> buscarAutor(String nombreAutor) throws Exception {
+        conectar();
+        List <Libro> libro= null;
+        try {
+            libro = em.createQuery("SELECT a FROM Libro a WHERE a.autor.nombre LIKE :nombre")
+                                   .setParameter("nombre", nombreAutor).getResultList();
+            return libro;
+        } catch (Exception e) {
+            System.out.println("No se encontró el autor del libro.");
+            throw e;        
+        } finally {
+            desconectar();
+        }    
+    }
+    
+    public List<Libro> buscarEdit(String nameEditorial) throws Exception {
+        conectar();
+        List <Libro> libro= null;
+        try {
+            libro = em.createQuery("SELECT li FROM Libro li WHERE li.editorial.nombre LIKE :nombre")
+                                   .setParameter("nombre", nameEditorial).getResultList();
+            return libro;
+        } catch (Exception e) {
+            System.out.println("No se encontró la editorial.");
+            throw e;        
+        } finally {
+            desconectar();
+        }    
+    }
     
 }
